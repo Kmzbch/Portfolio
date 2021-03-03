2a72b876f8168c3f556ad3603ba5ebe8c67c06f5
Kei Mizubuchi <41995658+Kmzbch@users.noreply.github.com>
2021-03-02 22:47:37 -0500

    TechStack modified

--- src/components/shared/TechStack.js
+++ src/components/shared/TechStack.js
@@ -1,58 +1,21 @@
+import { Paper } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { List, ListItem, ListItemIcon } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-	typography: {
-		padding: theme.spacing(2)
-	}
-}));
 
 export default function TechStack(props) {
-	const classes = useStyles();
-	const [ anchorEl, setAnchorEl ] = React.useState(null);
-
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-
-	const open = Boolean(anchorEl);
-	const id = open ? 'simple-popover' : undefined;
-
 	return (
 		<div className={'tech-stack-wrapper'}>
-			<div onMouseEnter={handleClick}>
+			<Paper>
 				<img className={'tech-stack-image'} src={props.src} title={props.title} alt={'Logo of ' + props.alt} />
-				<div className="middle-wrapper translate middle">
-					<span>{props.alt}</span>
+				<div
+					className="middle-wrapper translate middle"
+					style={{
+						borderTop: '1px solid #DDD',
+						borderRadius: '0'
+					}}
+				>
+					<div className={'tech-stack-description'}>{props.alt}</div>
 				</div>
-			</div>
-			{/* <Popover
-				style={{
-					minHeight: '10px'
-				}}
-				id={id}
-				open={open}
-				anchorEl={anchorEl}
-				onClose={handleClose}
-				anchorOrigin={{
-					vertical: 'top',
-					horizontal: 'center'
-				}}
-				transformOrigin={{
-					vertical: 'bottom',
-					horizontal: 'center'
-				}}
-			>
-				<Typography className={classes.typography}>{props.title}</Typography>
-			</Popover> */}
+			</Paper>
 		</div>
 	);
 }
