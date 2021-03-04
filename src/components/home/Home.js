93ba734e4bc0ccbb651e3939ad8035ebeffff805
Kei Mizubuchi <41995658+Kmzbch@users.noreply.github.com>
2021-03-04 11:33:37 -0500

    Home completed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,51 +1,30 @@
 import React, { Component } from 'react';
-import './Home.scss';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { ArrowForwardOutlined } from '@material-ui/icons';
+import './Home.scss';
 
 export default class Home extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { showComponent: true };
 	}
 
 	render() {
 		return (
 			<div id="home" className={'page home'}>
 				<div className={'content'}>
-					<div
-						style={{
-							paddingTop: '30vh',
-							fontFamily: 'Raleway, Sans Serif'
-						}}
-					>
-						<Typography
-							variant="h2"
-							style={{
-								whiteSpace: 'pre-line',
-								display: 'inline'
-							}}
-						>
-							Hi, I'm{' '}
-						</Typography>
-						<Typography
-							variant="h2"
-							style={{
-								whiteSpace: 'pre-line',
-								color: '#fbe77d',
-								display: 'inline'
-							}}
-						>
-							Kei Mizubuchi
-						</Typography>
-						<Typography variant="h3" style={{ opacity: '.6' }}>
-							Software Developer
-						</Typography>
-					</div>
+					<Typography variant="h2" component="span">
+						Hi, I'm{' '}
+					</Typography>
+					<Typography variant="h2" component="span" className="highlight">
+						Kei Mizubuchi
+					</Typography>
+					<Typography variant="h3" style={{ opacity: '.6' }}>
+						Software Developer
+					</Typography>
 
 					<Button
-						className={'view-button'}
+						className="view-button"
 						size="large"
 						variant="outlined"
 						href="#about"
