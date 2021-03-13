97c8d99a44262f7aaefc8e39fbeb2c632ccbea93
Kei Mizubuchi <41995658+Kmzbch@users.noreply.github.com>
2021-03-13 01:42:09 -0500

    AboutMe  content modified

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -17,21 +17,19 @@ export default class AboutMe extends Component {
 					</Typography>
 					<Typography style={{ fontSize: '1.1em', margin: '2vh auto' }}>
 						I am a software developer who completed a Software Engineering Diploma program at Centennial
-						College. I am currently seeking a software engineering/full-stack developer position where
-						reliability is valued.
+						College. I am currently seeking a software engineering position where reliability is valued.
 					</Typography>
 					<Typography style={{ fontSize: '1.1em', margin: '2vh auto' }}>
-						I have over five years of work experience in a university library as an On-Site IT Support
-						Programmer, in which I performed maintenance, troubleshooting, system backup and upgrade of
-						integrated library management system on LAMP servers. I closely communicated with the internal
-						team and library staff and helped them out with streamlining their business processes by
-						scripting in Shell script / Perl such as large-scale bibliographic record conversion.
+						I have five-year work experience in a university library as an On-Site IT Support Programmer, in
+						which I performed maintenance, troubleshooting, system update of library management system on
+						LAMP servers. Closely collaborating with the library staff and the team, I streamlined their
+						business processes by scripting such as bibliographic record conversion.
 					</Typography>
 					<Typography style={{ fontSize: '1.1em', margin: '2vh auto' }}>
-						In most recent positions in Canda, I have accumulated further experience in web application
-						development with various modern technologies, including but not limited to ASP.NET Core,
-						Angular, SQL Server stacks. My current focus are microservices and RESTful APIs but I am open to
-						work both the front-end and back-end.
+						In my most recent positions in Canda, I have accumulated further experience in web development
+						with various modern technologies, including but not limited to ASP.NET Core, Angular, SQL Server
+						stacks. My current focus is RESTful APIs but I am willing to work on both front-end and
+						back-end.
 					</Typography>
 				</Grid>
 				<Grid className="profile" item sm={12} md={5}>
